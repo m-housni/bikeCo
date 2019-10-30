f4144f1aee2bafd683358d947f035abc5009b7d0
Mohamed Housni <mohamed.housni@um5s.net.ma>
2019-10-30 20:18:24 +0100

    bikeco v1.0

---
+++ public/about.php
@@ -0,0 +1,18 @@
+<?php require_once('../private/initialize.php'); ?>
+
+<?php $page_title = 'About Us'; ?>
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+
+<div id="main">
+
+  <div id="page" class="narrow">
+    <h2>About Us</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis accusantium placeat voluptates consequatur? Ducimus dolore nemo quibusdam debitis quaerat deserunt.</p>
+    <p>Us dolore nobis a nemo laborum voluptatum ad repellendus consequuntur ut, sequi eum eveniet blanditiis alias placeat autem explicabo quaerat nihil dolorum. Maxime voluptatem odio soluta nulla asperiores error.</p>
+  </div>
+  
+</div>
+
+<?php $super_hero_image = 'AdobeStock_50633008_xlarge.jpeg'; ?>
+
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
