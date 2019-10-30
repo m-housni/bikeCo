334fca2a9da3e84b58902e77b7fb554c5a80be5a
Mohamed Housni <mohamed.housni@um5s.net.ma>
2019-10-30 22:05:35 +0100

    Update initialize.php

--- private/initialize.php
+++ private/initialize.php
@@ -39,7 +39,7 @@
   // Autoload class definitions
   function my_autoload($class) {
     if(preg_match('/\A\w+\Z/', $class)) {
-      include('classes/' . $class . '.class.php');
+      include(PRIVATE_PATH.'/classes/' . $class . '.class.php');
     }
   }
   spl_autoload_register('my_autoload');
