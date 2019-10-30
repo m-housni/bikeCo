9ec204bf46ebaaff94c4d3661f1b8cba2dff0c0a
Mohamed Housni <mohamed.housni@um5s.net.ma>
2019-10-30 23:45:07 +0100

    Update initialize.php

--- public/private/initialize.php
+++ public/private/initialize.php
@@ -8,7 +8,7 @@
   // dirname() returns the path to the parent directory
   define("PRIVATE_PATH", dirname(__FILE__));
   define("PROJECT_PATH", dirname(PRIVATE_PATH));
-  define("PUBLIC_PATH", PROJECT_PATH);
+  define("PUBLIC_PATH", PROJECT_PATH . '/public');
   define("SHARED_PATH", PRIVATE_PATH . '/shared');
 
 
